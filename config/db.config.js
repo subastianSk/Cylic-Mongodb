cfcf2585be13848a4d394f7bfffae9f962cc6fdd
subastianSk <subastian.sk@gmail.com>
2023-06-16 11:45:35 +0700

    done

--- config/db.config.js
+++ config/db.config.js
@@ -9,11 +9,9 @@
 //     config
 // };
 const config = {
-    url: `mongodb://${process.env.DB_HOST_DEV}:${process.env.DB_PORT_DEV}/${process.env.DB_NAME_DEV}`,
+    url: `mongodb+srv://${process.env.DB_USER_DEV}:${process.env.DB_PASSWORD_DEV}@${process.env.DB_HOST_DEV}/${process.env.DB_NAME_DEV}`,
     options: {
         authSource: process.env.DB_SOURCE_DEV,
-        user: process.env.DB_USER_DEV,
-        pass: process.env.DB_PASSWORD_DEV,
     }
 }
 
